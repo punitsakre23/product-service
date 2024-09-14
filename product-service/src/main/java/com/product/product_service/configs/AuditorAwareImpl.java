c617da68a7269008c4c9c0c3c64a5c5c6ee94a53
Punit Sakre <punitsakre786@gmail.com>
2024-09-15 02:02:37 +0530

    Resolved 401 Unauthorized HTTP status which was caused due to Auditing setup

--- product-service/src/main/java/com/product/product_service/configs/AuditorAwareImpl.java
+++ product-service/src/main/java/com/product/product_service/configs/AuditorAwareImpl.java
@@ -1,9 +1,10 @@
 package com.product.product_service.configs;
 
+import com.product.product_service.constants.ProductConstant;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.Optional;
 
@@ -12,14 +13,16 @@ public class AuditorAwareImpl implements AuditorAware<String> {
 
     @Override
     public Optional<String> getCurrentAuditor() {
+        return Optional.of(getCurrentUser());
+    }
 
-        // Use SecurityContextHolder to get the current authentication
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        // Return the username if authenticated; otherwise, return "SYSTEM"
-        return Optional.ofNullable(authentication)
-                .filter(Authentication::isAuthenticated)
-                .map(Authentication::getName)
-                .or(() -> Optional.of("SYSTEM"));
+    private String getCurrentUser() {
+        return Optional.ofNullable(RequestContextHolder.getRequestAttributes())
+                .filter(ServletRequestAttributes.class::isInstance)
+                .map(ServletRequestAttributes.class::cast)
+                .map(ServletRequestAttributes::getRequest)
+                .map(request -> request.getHeader("X-User-Name"))
+                .filter(userName -> !userName.isEmpty())
+                .orElse(ProductConstant.USER);
     }
 }
